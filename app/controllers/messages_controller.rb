fcd8c2ae09b1e04991cc3bc678c6ab116b5d3edd
wayungi <fwayungi@gmail.com>
2022-12-17 17:23:35 +0300

    FIXES: Linters

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,7 @@
 class MessagesController < ApplicationController
-
   # GET /messages
   def index
     @message = Message.all.sample
-    render json:  @message
+    render json: @message
   end
-
-  
 end
